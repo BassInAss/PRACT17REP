8b11458a29cf066fc1579e43816abd65ac93dd2b
Viktor <viktor.kononenko.2003@mail.ru>
2022-03-14 15:51:25 +0500

    created projected

---
+++ pract17Kon/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chromium;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace pract17Kon
+{
+    [TestFixture]
+    public class HeaderTest
+    {
+        IWebDriver webDriver = new ChromeDriver();
+        [TestCase]
+    }
+}
